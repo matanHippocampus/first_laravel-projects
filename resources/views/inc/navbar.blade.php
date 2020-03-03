4414057ef8cc5191fe30e8d9db872e1ac295210b
Lior Peleg <matan@hippocampus.me>
2020-03-03 17:47:29 +0200

    first commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,12 @@
+    <div>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <span class="navbar-brand">{{config('app.name')}}</span>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <a class="nav-link" href="/">Home</a>
+                <a class="nav-link" href="/about">About</a>
+                <a class="nav-link" href="/services">Services</a>
+            </ul>
+          </div>
+        </nav>
+    </div>
